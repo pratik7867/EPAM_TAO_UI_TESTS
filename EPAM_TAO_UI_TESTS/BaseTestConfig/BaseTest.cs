579c643080afc7f4089e7d031c25c17683c3017a
Pratik_Pandya <Pratik_Pandya@epam.com>
2022-10-16 11:53:13 +0530

    Add project files.

---
+++ EPAM_TAO_UI_TESTS/BaseTestConfig/BaseTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Configuration;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using EPAM_TAO_CORE_UI_TAF.TestSetup;
+using EPAM_TAO_CORE_UI_TAF.UI_Helpers;
+using System.Reflection;
+using EPAM_TAO_UI_POM.Pages.Products;
+using EPAM_TAO_UI_POM.Pages.Cart;
+using EPAM_TAO_UI_POM.Pages.Checkout;
+
+namespace EPAM_TAO_UI_TESTS.BaseTestConfig
+{
+    [TestFixture]
+    public abstract class BaseTest : TestHookup
+    {
+        private string strBrowser { get { return ConfigurationManager.AppSettings["Browser"].ToString(); } }
+        private string strSiteURL { get { return ConfigurationManager.AppSettings["SiteURL"].ToString(); } }
+        private string strAUT { get { return ConfigurationManager.AppSettings["AUT"].ToString(); } }
+        protected string strUserName { get { return ConfigurationManager.AppSettings["UserName"].ToString(); } }
+        protected string strPassword { get { return ConfigurationManager.AppSettings["Password"].ToString(); } }
+        protected Exception testEx { get; set; }
+        protected ProductsPage productsPage {get; set;}
+        protected CartPage cartPage { get; set; }
+        protected CheckoutPage checkoutPage { get; set; }
+
+        [SetUp]
+        protected void setupDriverSession()
+        {
+            try
+            {
+                driver = InitBrowser((BrowserType)Enum.Parse(typeof(BrowserType), strBrowser.ToUpper()));
+                CommonUtilities.commonUtilities.NavigateToURL(driver, strSiteURL);
+                CommonUtilities.commonUtilities.MaximizeWindow(driver);
+
+                ExtentReportHelper.GetInstance(strAUT, driver).CreateTest(TestContext.CurrentContext.Test.Name);
+            }
+            catch(Exception ex)
+            {
+                ErrorLogger.errorLogger.ErrorLog(MethodBase.GetCurrentMethod().Name, ex, driver);
+                ExtentReportHelper.GetInstance(strAUT, driver).SetTestStatusFail($"<br>{ex.Message}<br>Stack Trace: <br>{ex.StackTrace}<br>");
+            }
+        }
+
+        [TearDown]
+        protected void closeDriverSession()
+        {
+            try
+            {
+                var status = TestContext.CurrentContext.Result.Outcome.Status;                
+
+                switch (status)
+                {
+                    case TestStatus.Failed:
+                        var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+                        var errorMessage = "<pre>" + TestContext.CurrentContext.Result.Message + "</pre>";
+
+                        ErrorLogger.errorLogger.ErrorLog(MethodBase.GetCurrentMethod().Name, testEx, driver);
+                        ExtentReportHelper.GetInstance(strAUT, driver).SetTestStatusFail($"<br>{errorMessage}<br>Stack Trace: <br>{stacktrace}<br>", CommonUtilities.commonUtilities.TakeScreenshot(driver, TestContext.CurrentContext.Test.Name));
+                        break;
+                    case TestStatus.Skipped:
+                        ExtentReportHelper.GetInstance(strAUT, driver).SetTestStatusSkipped();
+                        break;
+                    default:
+                        ExtentReportHelper.GetInstance(strAUT, driver).SetTestStatusPass();
+                        break;
+                }                
+            }
+            catch(Exception ex)
+            {
+                ErrorLogger.errorLogger.ErrorLog(MethodBase.GetCurrentMethod().Name, ex, driver);
+                ExtentReportHelper.GetInstance(strAUT, driver).SetTestStatusFail($"<br>{ex.Message}<br>Stack Trace: <br>{ex.StackTrace}<br>");
+            }
+            finally
+            {
+                CloseBrowser();
+            }
+        }
+
+        [OneTimeTearDown]
+        protected void CloseAll()
+        {
+            try
+            {
+                ExtentReportHelper.GetInstance(strAUT, driver).CloseExtentReport();
+            }
+            catch(Exception ex)
+            {
+                ErrorLogger.errorLogger.ErrorLog(MethodBase.GetCurrentMethod().Name, ex);
+                ExtentReportHelper.GetInstance(strAUT, driver).SetTestStatusFail($"<br>{ex.Message}<br>Stack Trace: <br>{ex.StackTrace}<br>");
+            }
+        }
+    }
+}
---
+++ EPAM_TAO_UI_TESTS/UITests/Checkout/ProductCheckoutWorkFlow.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Configuration;
+using NUnit.Framework;
+using EPAM_TAO_UI_TESTS.BaseTestConfig;
+using EPAM_TAO_UI_POM.Pages.Login;
+
+namespace EPAM_TAO_UI_TESTS.UITests.Checkout
+{
+    [TestFixture]
+    public class ProductCheckoutWorkFlow : BaseTest
+    {
+        [Test]
+        public void ProductCheckoutTest()
+        {
+            try
+            {
+                //DATA Arrangements
+                var productName = ConfigurationManager.AppSettings["ProductName"].ToString();
+                var firstName = ConfigurationManager.AppSettings["FirstName"].ToString();
+                var lastName = ConfigurationManager.AppSettings["LastName"].ToString();
+                var postalCode = ConfigurationManager.AppSettings["PostalCode"].ToString();
+
+                //Log in to application
+                productsPage = LogInPage.GetInstance(driver).LogIntoApplication(strUserName, strPassword);
+
+                //Add Product to Cart
+                var productPrice = productsPage.getProductPrice(productName);
+                productsPage.AddToCart(productName);
+
+                //Navigate to Cart Page and Validate Price of the added Product
+                cartPage = productsPage.ClickOnShoppingCart();
+                Assert.AreEqual(productPrice, cartPage.GetProductPrice());                
+
+                //Navigate to Checkout Page and Fill up mandatory details to conitune
+                checkoutPage = cartPage.ClickOnCheckout();
+                checkoutPage.FillUpCheckoutDetailsAndContiue(firstName, lastName, postalCode);
+
+                //Validate Name and Price of the added Product
+                Assert.AreEqual(productName, checkoutPage.GetProductName());
+                Assert.AreEqual(productPrice, checkoutPage.GetProductPrice());
+
+                //Click on Finish button
+                checkoutPage.ClickOnFinish();
+            }
+            catch(Exception ex)
+            {
+                testEx = ex;
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
